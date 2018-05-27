655c15da37d30970e92709b80ee7ba9dca175914
teppeis <teppeis@gmail.com>
2018-05-28 00:44:18 +0900

    feat: convert rules

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 const {Linter} = require('eslint');
 const Config = require('eslint/lib/config');
 const {loadObject} = require('eslint/lib/config/config-file');
+const oEntries = require('object.entries');
 
 function importESLintConfig(config) {
   const {rules = {}} = loadESLintConfig(config);
+  const tsConfig = convertESLintRulesToTSLintConfig(rules);
   return {
-    rules,
+    rules: tsConfig.rules,
   };
 }
 
@@ -22,4 +24,42 @@ function loadESLintConfig(config) {
   return loadObject(config, context);
 }
 
+function convertESLintRulesToTSLintConfig(rules) {
+  const tsRules = oEntries(rules)
+    .map(convertESLintRule)
+    .filter(([name, value]) => name && value)
+    .reduce((prev, [name, value]) => {
+      prev[name] = value;
+      return prev;
+    }, {});
+
+  return {
+    rules: tsRules,
+  };
+}
+
+function convertESLintRule([name, value]) {
+  if (Array.isArray(value)) {
+    // TODO: extract options
+    value = value[0];
+  }
+
+  if (value === 'off' || value === 0) {
+    return [null, null];
+  } else if (value === 'warn' || value === 1) {
+    value = 'warning';
+  } else if (value === 'error' || value === 2) {
+    value = 'error';
+  } else {
+    throw new Error(`invalid rule setting: ${name}, ${value}`);
+  }
+
+  return [es2ts[name], {severity: value}];
+}
+
+const es2ts = {
+  'no-cond-assign': 'no-conditional-assignment',
+};
+
 module.exports = importESLintConfig;
+module.exports.convertESLintRulesToTSLintConfig = convertESLintRulesToTSLintConfig;
--- test/index.js
+++ test/index.js
@@ -1,14 +1,56 @@
 'use strict';
 
-const sut = require('../');
 const assert = require('assert');
+const sut = require('../');
+const {convertESLintRulesToTSLintConfig: convert} = sut;
 
 describe('tslint-import-eslint-config', () => {
   it('should be a function', () => {
     assert(typeof sut === 'function');
   });
   it('should return an empty object as `rules` prop for empty config', () => {
-    const result = sut({});
-    assert.deepEqual(result.rules, {});
+    assert.deepEqual(sut({}), {rules: {}});
+  });
+});
+
+describe('convertESLintRulesToTSLintConfig', () => {
+  it('should be a function', () => {
+    assert(typeof convert === 'function');
+  });
+  describe('should convert a ESLint rule to the equivalent TSLint rule', () => {
+    it('error', () => {
+      const actual = convert({'no-cond-assign': 'error'});
+      assert.deepEqual(actual, {rules: {'no-conditional-assignment': {severity: 'error'}}});
+    });
+    it('warn', () => {
+      const actual = convert({'no-cond-assign': 'warn'});
+      assert.deepEqual(actual, {rules: {'no-conditional-assignment': {severity: 'warning'}}});
+    });
+    it('2', () => {
+      const actual = convert({'no-cond-assign': 2});
+      assert.deepEqual(actual, {rules: {'no-conditional-assignment': {severity: 'error'}}});
+    });
+    it('1', () => {
+      const actual = convert({'no-cond-assign': 1});
+      assert.deepEqual(actual, {rules: {'no-conditional-assignment': {severity: 'warning'}}});
+    });
+    it('[error]', () => {
+      const actual = convert({'no-cond-assign': ['error']});
+      assert.deepEqual(actual, {rules: {'no-conditional-assignment': {severity: 'error'}}});
+    });
+  });
+  describe('should filter disabled ESLint rules', () => {
+    it('off', () => {
+      const actual = convert({'no-cond-assign': 'off'});
+      assert.deepEqual(actual, {rules: {}});
+    });
+    it('0', () => {
+      const actual = convert({'no-cond-assign': 0});
+      assert.deepEqual(actual, {rules: {}});
+    });
+  });
+  it('should filter a ESLint rule if it has no equivalent TSLint rule', () => {
+    const actual = convert({'foo-bar-baz': 'error'});
+    assert.deepEqual(actual, {rules: {}});
   });
 });
